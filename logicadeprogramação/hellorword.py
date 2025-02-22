48d5eab9bf91cc1b7085cf783e9fd8ef70ebe4a0
MatheusDeZottis <matheusdezottis@gmail.com>
2025-02-22 11:58:09 -0300

    Primeira aula de lógica

---
+++ logicadeprogramação/hellorword.py
@@ -0,0 +1,5 @@
+# Exibindo a mensagem do viajante do tempo
+
+print("Olá, Humano do século XXI!")
+print("Meu nome é cronos é venho do ano 3023")
+print("Minha missão é de é estudar como a tecnologia moldou, o mundo ao logo dos séculos")
