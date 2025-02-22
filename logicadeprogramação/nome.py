44875a0a8db97e6dce1e4ec9618e9d545a799845
MatheusDeZottis <matheusdezottis@gmail.com>
2025-02-22 13:14:15 -0300

    segunda aula de lógica

---
+++ logicadeprogramação/nome.py
@@ -0,0 +1,9 @@
+#Solicitar o nome da pesssoa do presente 
+
+nome_pessoa = input(" Viajante do tempo: Olá, humano do seculo XX !! Qual é o seu nome? ")
+
+# reposta personalizada
+
+print(f"Pessoa do presente : Olá, Cronos! Meu nome é {nome_pessoa} quem é voce?")
+print("Pessoa do presente: Como você chegou até aqui?")
+print(F"Viajante do tempo Prazer em conhecẽlo,, {nome_pessoa}minha missão é estudar como a tecnologia")
